8b423d1cb67f59526f3b2b065537a7d44d9562b3
David White <david.white@trustpower.co.nz>
2022-07-31 13:36:02 +1200

    Testing removing typedef

--- RedisServer.js
+++ RedisServer.js
@@ -1,14 +1,6 @@
 'use strict';
 
-/**
- * Configuration options for a {@link RedisServer}.
- * @typedef {Object} RedisServer~Config
- * @property {String} [bin=redis-server]
- * @property {String} [conf=""]
- * @property {Boolean} [sentinel=false]
- * @property {(Number|String)} [port=6379]
- * @property {(String)} [replicaof]
- */
+
 
 /**
  * Invoked when an operation (i.e. {@link RedisServer#open}) completes.
